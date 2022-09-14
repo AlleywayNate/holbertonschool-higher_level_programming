2412d04f1e7e846b47bbf40461cfa4c8ac6ae505
Nathen Williams <nathenwilliams600@gmail.com>
2022-09-13 22:51:41 -0500

    fixed

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python3
-"""defines a Rectangle"""
+""" documentation """
 
 
-class Rectangle:
-    """Rectangle class"""
-    pass
+class Rectangle():
+    """ documentation	"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+        
--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-"""define a Rectangle"""
+""" documentation """
 
 
-class Rectangle:
-    """Rectangle class"""
-
+class Rectangle():
+    """ documentation	"""
     def __init__(self, width=0, height=0):
-        """init for Rectangle"""
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """Rectangle width getter"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Rectangle width setter"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self.width = width
 
     @property
     def height(self):
-        """Rectangle height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Rectangle height setter"""
-        if type(value) != int:
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        else:
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.__width * 2)
